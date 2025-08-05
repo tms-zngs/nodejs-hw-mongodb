dfb76bd8911035b3b208cae55a9c63f60d268d72
Tomas Zingis <thebash2344@gmail.com>
2025-08-05 18:19:00 +0200

    index fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoDB } from './db/initMongoDB.js';
-import { startServer } from './server.js';
+import { setupServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoDB();
-  startServer();
+  setupServer();
 };
 
 bootstrap();
