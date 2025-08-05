269c0204bf84403043ba29aa8cbaf9b3d31ad805
Tomas Zingis <thebash2344@gmail.com>
2025-08-05 18:16:15 +0200

    fix import

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/contacts.js';
+import { ContactsCollection } from '../db/models/contact.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
