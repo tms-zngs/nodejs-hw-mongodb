d3762770cab6a3e33906167bed3debd7665754fb
Tomas Zingis <thebash2344@gmail.com>
2025-08-05 18:02:27 +0200

    hw-done

---
+++ src/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts } from './services/contacts.js';
+import { getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', 3000));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(pino({ transport: { target: 'pinno-pretty' } }));
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+      message: 'Successfully found contacts!',
+    });
+  });
+
+  app.get('contacts/:id', async (req, res, next) => {
+    const id = req.params;
+    const contact = await getContactById(id);
+
+    if (!contact) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+
+    res.status(200).json({
+      data: contact,
+      message: `Successfully found contact with id ${id}!`,
+    });
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+  });
+};
