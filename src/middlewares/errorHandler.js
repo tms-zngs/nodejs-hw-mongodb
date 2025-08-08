e7bca2af44efa4db9237b2c8957825f0c739b4c7
Tomas Zingis <thebash2344@gmail.com>
2025-08-08 18:40:25 +0200

    update import one more

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-import { HttpError } from '../helpers/HttpError.js';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
