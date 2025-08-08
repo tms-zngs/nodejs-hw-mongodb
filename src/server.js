93eb0a275d4a332456cc92a99ce4d83e04e07460
Tomas Zingis <thebash2344@gmail.com>
2025-08-08 18:44:26 +0200

    router import

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { router } from './routers/contacts.js';
+import router from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
