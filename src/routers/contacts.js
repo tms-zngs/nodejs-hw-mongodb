c8ec52d01c7208d85a85e004419572f6055b8193
Tomas Zingis <thebash2344@gmail.com>
2025-08-08 18:37:01 +0200

    fix import

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,7 +6,7 @@ import {
   deleteContactController,
   patchContactController,
 } from '../controllers/contacts.js';
-import { ctrlWrapper } from '../helpers/ctrlWrapper.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = new Router();
 
